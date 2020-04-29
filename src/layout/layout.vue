0e778536189f6d73fc5f81dbf09a85aa72641b7f
hwz <87683202@qq.com>
2020-04-29 13:53:52 +0800

    1、增加登录页面和路由功能 2、增加首页（暂未落实实现方案）

---
+++ src/layout/layout.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>sssssssssssss</h1>
+</template>
+
+<script>
+    export default {
+        name: "layout"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Cookies from 'js-cookie'
+//element-ui 的UI配置
+import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui'
+Vue.use(ElementUI, {
+  size: Cookies.get('size') || 'small' // set element-ui default size
+})
 
 Vue.config.productionTip = false
 
+router.beforeEach((to, from, next) => {
+  // 记录访问的最后一个页面
+  if (from.path !== '/login') {
+    window.localStorage.setItem('from_path', from.path)
+  }
+  if (to.path == '/login') {
+    window.localStorage.removeItem('userInfo')
+  }
+  next()
+})
+
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App)
