8eb075ce8188d5159ec25b64d5ef3bdf90d5b736
hwz <87683202@qq.com>
2020-05-21 11:10:22 +0800

    1、动态列实现

--- src/views/commonview/YjTableWithToolsExpand.vue
+++ src/views/commonview/YjTableWithToolsExpand.vue
@@ -76,10 +76,14 @@
                             />
                         </el-button>
 
-                        <el-checkbox :indeterminate="isIndeterminate" v-model='cheackAllChecked' @change='checkAllFun'>全选</el-checkbox>
+                        <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">
+                            全选
+                        </el-checkbox>
                         <div style="margin: 15px 0;"></div>
-                        <el-checkbox v-for="(item,index) in List" :key="index"  v-model='checkArr' :label=item.id  @change="changevalue">{{item.name}}</el-checkbox>
-
+                        <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange">
+                            <el-checkbox v-for="city in cities" :label="city" :key="city.prop">{{city.label}}
+                            </el-checkbox>
+                        </el-checkbox-group>
                     </el-popover>
 
                 </el-button-group>
@@ -88,15 +92,13 @@
         <yj-table ref="yjtable" :search-content="input" :table-size="tableSize"
                   :is-show-row-do-something="isShowRowDoSomething"
                   :is-can-sort="isCanSort"
-                  :row-header="rowHeader" :data="data" :is-show-index="isShowIndex"
+                  :row-header="copyHeardSend" :data="data" :is-show-index="isShowIndex"
                   :is-show-selection="isShowSelection"/>
     </div>
 </template>
 
 <script>
     import YjTable from "./YjTable";
-
-    const cityOptions = ['上海', '北京', '广州', '深圳'];
     export default {
         name: "YjTableWithToolsExpand",
         components: {YjTable},
@@ -156,115 +158,38 @@
             return {
                 rowsList: [],
                 input: '',//模糊查询的内容
-
+                copyHeardSend: [],
                 copyHeard: [],
-
-                checkAll: false,
-                checkedCities: ['上海', '北京'],
-                cities: cityOptions,
-                isIndeterminate: true,
-
-                List: [
-                    {
-                        id: "1",
-                        name:'香蕉'
-                    },
-                    {
-                        id: "2",
-                        name:'苹果'
-                    },
-                    {
-                        id: "3",
-                        name:'橙子'
-                    },
-                    {
-                        id: "4",
-                        name:'鸭梨'
-                    },
-                    {
-                        id: "5",
-                        name:'葡萄'
-                    }
-                ],
-                cheackAllChecked: false,
-                checkArr: [],
-                checkCompleteArr: []
+                checkAll: true,
+                checkedCities: [],
+                cities: [],
+                isIndeterminate: false
             }
         },
-        watch: {},
+        watch: {
+
+        },
         mounted() {
             this.init()
-            var _this = this;
-            this.List.forEach(function(item, index) {
-                if (index >= 0) {
-                    _this.checkCompleteArr.push(item.id);
-                }
-            });
         },
         methods: {
             init() {
+                this.copyHeardSend = this.rowHeader
                 this.copyHeard = this.rowHeader
-                this.checkedCities = this.rowHeader
+                this.cities = this.copyHeard
+                this.checkedCities = this.copyHeard
             },
             handleCheckAllChange(val) {
-                this.checkedCities = val ? cityOptions : [];
+                this.checkedCities = (val ? this.copyHeard : []);
                 this.isIndeterminate = false;
+                this.copyHeardSend = this.checkedCities
             },
             handleCheckedCitiesChange(value) {
                 let checkedCount = value.length;
                 this.checkAll = checkedCount === this.cities.length;
                 this.isIndeterminate = checkedCount > 0 && checkedCount < this.cities.length;
-            },
-            //实现全选
-            checkAllFun() {
-                var _this = this;
-                this.checkArr = [];
-                if (this.cheackAllChecked) {
-                    this.List.forEach(function(item, index) {
-                        if (index >= 0) {
-                            _this.checkArr.push(item.id);
-                        }
-                    });
-                }
-            },
-            //实现反选
-            reverseChecked() {
-                var changeArr = [];
-                this.List.forEach(function(item, index) {
-                    if (index >= 0) {
-                        changeArr.push(item.id);
-                    }
-                });
-                this.removeByValue(changeArr, this.checkArr);
-            },
-            //反选过滤函数
-            removeByValue(changearr, checkarr) {
-                var _this = this;
-                for (var j = 0; j < checkarr.length; j++) {
-                    for (var i = 0; i < changearr.length; i++) {
-                        if (changearr[i] === checkarr[j]) {
-                            changearr.splice(i, 1);
-                            break;
-                        }
-                    }
-                }
-                this.checkArr = changearr;
-                if (changearr.length <= _this.checkCompleteArr.length) {
-                    this.cheackAllChecked = false;
-                }
-                if (changearr.length === _this.checkCompleteArr.length) {
-                    this.cheackAllChecked = true;
-                }
-            },
-//选项的change事件
-            changevalue() {
-                var _this = this;
-                if (this.checkArr.length <= _this.checkCompleteArr.length) {
-                    this.cheackAllChecked = false;
-                }
-                if (this.checkArr.length === _this.checkCompleteArr.length) {
-                    this.cheackAllChecked = true;
-                }
+                this.copyHeardSend = value
+
             }
 
         }
